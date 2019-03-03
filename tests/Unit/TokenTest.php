0ce3389d0d4f654dfc8acdf519b947fe7fbdb91e
Abdelrahman Omran <me@omranic.com>
2019-03-03 20:04:41 +0400

    Apply PHPUnit 8 updates

--- tests/Unit/TokenTest.php
+++ tests/Unit/TokenTest.php
@@ -19,7 +19,7 @@ class TokenTest extends TestCase
     /** @var \Rinvex\Authy\Response */
     protected $authyResponse;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
